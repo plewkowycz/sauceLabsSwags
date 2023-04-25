0b37df7c1c3a530c6129344094d453cd0a03346e
Piotr Lewkowicz <86957981+plewkowycz@users.noreply.github.com>
2023-04-25 04:30:08 +0200

    Delete cypress directory

--- cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add("login", (username, password) => {
-  cy.testid("username").type(`${username}`);
-  cy.testid("password").type(`${password}`);
-  cy.testid("login-button").click();
-});
-
-cy.testid = (selector_id) => {
-  return cy.get(`[data-test=${selector_id}]`);
-};
