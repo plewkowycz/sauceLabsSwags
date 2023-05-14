633eab8f48de5ca8c5ff02d66d9ebf110d111719
plewkowycz <piotr.lewkowycz@gmail.com>
2023-05-14 12:11:47 +0200

    Adjusting tests and tests cases according to suggestions

--- cypress/e2e/login_invalid_credentials.cy.js
+++ cypress/e2e/login_invalid_credentials.cy.js
@@ -1,17 +1,25 @@
+const locators = {
+  error: "[data-test=error]",
+};
+
+const userData = {
+  incorrectUsername: "standard_user_incorrect",
+  incorrectPassword: "catdogcat",
+};
+
+const errorMessages = {
+  credentialsMismatch:
+    "Epic sadface: Username and password do not match any user in this service",
+};
+
 describe("Sauce Demo Invalid Login", () => {
   it("Displays an error message for invalid credentials", () => {
     cy.visit("https://www.saucedemo.com");
 
-    const user_correct = "standard_user_incorrect";
-    const password_incorrect_long = "catdogcat";
+    cy.login(userData.incorrectUsername, userData.incorrectPassword);
 
-    cy.login(user_correct, password_incorrect_long);
-
-    cy.testid("error")
+    cy.get(locators.error)
       .should("be.visible")
-      .and(
-        "contain",
-        "Epic sadface: Username and password do not match any user in this service"
-      );
+      .and("contain", errorMessages.credentialsMismatch);
   });
 });
--- cypress/e2e/purchase_flow_with_valid_information.cy.js
+++ cypress/e2e/purchase_flow_with_valid_information.cy.js
@@ -1,36 +1,65 @@
+const locators = {
+  username: "[data-test=username]",
+  password: "[data-test=password]",
+  loginButton: "[data-test=login-button]",
+  addToCart: "[data-test=add-to-cart-sauce-labs-backpack]",
+  shoppingCart: "#shopping_cart_container",
+  cartItem: ".cart_item",
+  checkout: "[data-test=checkout]",
+  firstName: "[data-test=firstName]",
+  lastName: "[data-test=lastName]",
+  postalCode: "[data-test=postalCode]",
+  continue: "[data-test=continue]",
+  summaryInfo: ".summary_info",
+  summaryTaxLabel: ".summary_tax_label",
+  summaryTotalLabel: ".summary_total_label",
+  finish: "[data-test=finish]",
+  backToProducts: "[data-test=back-to-products]",
+};
+
+const userData = {
+  validUsername: "standard_user",
+  validPassword: "secret_sauce",
+  firstName: "Jon",
+  lastName: "Snow",
+  postalCode: "12345",
+};
+
+const purchaseInfo = {
+  itemTotal: "29.99",
+  tax: "2.40",
+  total: "32.39",
+};
+
 describe("Sauce Demo Purchase Flow", () => {
   it("Completes the purchase flow successfully", () => {
-    const item_total = "29.99";
-    const tax = "2.40";
-    const total = "32.39";
-
     cy.visit("https://www.saucedemo.com");
 
-    cy.testid("username").type("standard_user");
-    cy.testid("password").type("secret_sauce");
-    cy.testid("login-button").click();
+    cy.get(locators.username).type(userData.validUsername);
+    cy.get(locators.password).type(userData.validPassword);
+    cy.get(locators.loginButton).click();
 
-    cy.testid("add-to-cart-sauce-labs-backpack").click();
+    cy.get(locators.addToCart).click();
 
-    cy.get("#shopping_cart_container").click();
+    cy.get(locators.shoppingCart).click();
 
-    cy.get(".cart_item")
+    cy.get(locators.cartItem)
       .should("have.length", 1)
       .and("contain", "Sauce Labs Backpack");
 
-    cy.testid("checkout").click();
+    cy.get(locators.checkout).click();
 
-    cy.testid("firstName").type("Jon");
-    cy.testid("lastName").type("Snow");
-    cy.testid("postalCode").type("12345");
-    cy.testid("continue").click();
+    cy.get(locators.firstName).type(userData.firstName);
+    cy.get(locators.lastName).type(userData.lastName);
+    cy.get(locators.postalCode).type(userData.postalCode);
+    cy.get(locators.continue).click();
 
-    cy.get(".summary_info").should("contain", item_total);
-    cy.get(".summary_tax_label").should("contain", tax);
-    cy.get(".summary_total_label").should("contain", total);
+    cy.get(locators.summaryInfo).should("contain", purchaseInfo.itemTotal);
+    cy.get(locators.summaryTaxLabel).should("contain", purchaseInfo.tax);
+    cy.get(locators.summaryTotalLabel).should("contain", purchaseInfo.total);
 
-    cy.testid("finish").click();
+    cy.get(locators.finish).click();
 
-    cy.testid("back-to-products").should("be.visible");
+    cy.get(locators.backToProducts).should("be.visible");
   });
 });
